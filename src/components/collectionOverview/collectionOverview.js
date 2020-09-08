2bacc490053a5a6d7f50c09d069c4ed233343913
unknown <souvikelric@gmail.com>
2020-09-09 04:46:10 +0530

    Modified shop-page

--- src/components/collectionOverview/collectionOverview.js
+++ src/components/collectionOverview/collectionOverview.js
@@ -5,22 +5,25 @@ import { selectShopItems } from "../../redux/shop/shopSelectors";
 import { connect } from "react-redux";
 import Preview from "../../components/preview-collection/preview";
 
-const CollectionOverview = ({ collections }) => (
-  <div className="collection-overview">
-    {collections.map((collection) => {
-      return (
-        <Preview
-          key={collection.id}
-          items={collection.items}
-          title={collection.title}
-        />
-      );
-    })}
-  </div>
-);
+const CollectionOverview = ({ collections }) => {
+  console.log(collections);
+  return (
+    <div className="collection-overview">
+      {collections.map((collection) => {
+        return (
+          <Preview
+            key={collection.id}
+            items={collection.items}
+            title={collection.title}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopItems,
+const mapStateToProps = ({ shop }) => ({
+  collections: shop.collections,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
