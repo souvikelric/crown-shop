8e9d1d74d551a04d98f4a2f781fb5664967c932e
unknown <souvikelric@gmail.com>
2020-09-10 01:17:17 +0530

    Probable fix

--- src/pages/categories/categoryPage.js
+++ src/pages/categories/categoryPage.js
@@ -1,36 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-import { selectCollection } from "../../redux/shop/shopSelectors";
+
 import CollectionItem from "../../components/collection-item/collection-item";
+
+import { selectCollection } from "../../redux/shop/shopSelectors";
 
 import "./categoryPage.scss";
 
-const CategoryPage = ({ collection, match }) => {
-  console.log(collection);
+const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="category-page">
-      {/* <h1>{collection.title.toUpperCase()}</h1> */}
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
       <div className="items">
         {items.map((item) => (
-          <CollectionItem item={item} key={item.id} />
+          <CollectionItem key={item.id} item={item} />
         ))}
       </div>
     </div>
   );
 };
 
-const helper = {
-  hats: 0,
-  sneakers: 1,
-  jackets: 2,
-  womens: 3,
-  mens: 4,
-};
-
 const mapStateToProps = (state, ownProps) => ({
-  collection:
-    state.shop.collections[helper[ownProps.match.params.collectionId]],
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
-export default connect(mapStateToProps)(CategoryPage);
+export default connect(mapStateToProps)(CollectionPage);
